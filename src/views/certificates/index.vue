dec7fba8d40cf228e9777c1d40b040db1494e321
张浩 <zhanghaoyf@zhangyue.com>
2020-06-02 13:54:52 +0800

    1、搜索必填 2、证书列表页样式修改

--- src/views/certificates/index.vue
+++ src/views/certificates/index.vue
@@ -1,18 +1,20 @@
 <template>
     <div class="certificates">
-        <section class="single" v-for="(item,index) in list" :key="index">
-            <a class="img"></a>
-            <div class="info" @click="goDetail(item.id,item.custId)">
-                <!-- <p class="g_title_style">{{custName}}-{{item.certificateTypeName}}</p> -->
-                <p class="g_title_style">{{custName}}</p>
-                <p>{{item.createTime}}</p>
-            </div>
-            <p class="more">
-                阅读更多
-                <span></span>
-            </p>
-        </section>
-
+        <!-- 间隔线 -->
+        <p class="line"></p>
+        <!-- 搜索结果列表 -->
+        <div class="lists">
+            <p class="title_res">{{totalCount}} 搜索结果：</p>
+            <template v-if="list.length">
+                <section class="single"  v-for="(item,index) in list" :key="index">
+                        <div class="index">{{index+1}}</div>
+                        <div class="info" @click="goDetail(item.id,item.custId)">
+                            <p class="g_title_style">{{custName}}</p>
+                            <p>{{item.createTime}}</p>
+                        </div>
+                </section>
+            </template>
+        </div>
         <!-- 分页 -->
         <div class="pagination" v-if="list.length">
             <el-pagination
@@ -86,79 +88,61 @@ export default {
     .certificates {
         margin: 0 50px;
     }
-.pagination {
-        margin: 60px 0 30px 0;
+
+    .title {
+        max-width: 300px;
+        margin: 50px 0 0 0;
+        padding-left: 30px;
     }
-.single {
-    cursor: pointer;
-    border-bottom: 1px solid #e1e1e1;
-    .img {
-        width: 81px;
-        height: 81px;
-        overflow: hidden;
-        border-radius: 4px;
-        float: left;
-        margin: 6px 0 0 0;
-        position: relative;
-        text-align: center;
-        line-height: 81px;
-        position: relative;
-        display: block;
-        background: #919191;
-        color: #ffffff;
-        transition: all 0.3s ease-out;
-        background-image: url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAnFBMVEUAAAD///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////+TINBkAAAAM3RSTlMAVaqA+O/cKwXmklvq1MWvoYdPSjcWEAv78+HY0c28nIxwZUZCMCMgv6U7M7eXbicbeHLKc4THAAABhklEQVRYw+3WyY6CUBAF0FJAEQccQJwVFXBq7e77///Wr0CJ2yqSZuNdkMtLDpDiJUCf/H/Cg3Wc6nm8AmfQ1fHExysbjW84uT3ZfOwofO7uGUVjbpbK3x55n3LfKzyWz/sGpvflnrNxy/NM6psLPs4TXohsoCH15DYGXEJeOgG/Us/TMw795xgvYs9Zm+4RxYDjyj2nB6CbOICv88S7aOsDvbbGczygxftI6ykDZ6j2xRybeu+yP1fwq2rPH1X1fX6Jej+t6MMlgK/6/DfvP68+n7Hf6X26qOYf7Ed63+7U6xP2lt5fe9V8UNU71fxF4In3Wsub0FuOEn9Akd2bnws87YAeOIPykiJPK8Brj2yYFAsTW+SD4nt75UkMFZ4sYJaXgXF3ovFM5tn5ReM7T8Q+5a92UfemnqWeYgPcZx+aLvV0e/t9HSt8ZERc1PaoI/fUNSRlbS0AhacmsDZ6Ceg8zY3pv/Tm5yr1B5Tx9ynJYxXYPndDsS1n6GwnLqkTWEf6pLb8Ae29ZJq4wKspAAAAAElFTkSuQmCC');
-        background-size: 26px;
-        background-repeat: no-repeat;
-        background-position: center;
-    }
-    a:hover {
-        background-color: #cf1b15;
-        color: #ffffff;
-        border-color: #ad0903;
-    }
-    .g_title_style {
-        font-size: 24px;
-        margin: 10px 0;
-    }
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 40px;
-    padding: 40px 0 50px 0;
-    .index {
-        flex-shrink: 0;
-        width: 44px;
-        height: 44px;
-        line-height: 44px;
-        text-align: center;
-        border-radius: 22px;
-        background-color: #fcfcfc;
+    .satisfied {
         font-size: 13px;
-        color: #a7a7a7;
-        box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.2);
-        margin-right: 15px;
+        color: #e1e1e1;
     }
-    .info {
-        font-size: 12px;
+
+    .line {
+        background: #e1e1e1;
+        width: 100%;
+        height: 1px;
+        margin: 44px 0 40px 0 !important;
+    }
+
+    .lists {
+        padding-left: 30px;
+        margin: auto;
+    }
+    .title_res {
+        font-size: 14px;
+        font-weight: bold;
         color: #919191;
+        margin-bottom: 24px;
     }
-    .more {
+    .single {
         display: flex;
         align-items: center;
-        font-size: 15px;
-        color: #cf1b15;
-        width: 100%;
+        margin-bottom: 40px;
+        cursor: pointer;
+        .index {
+            flex-shrink: 0;
+            width: 44px;
+            height: 44px;
+            line-height: 44px;
+            text-align: center;
+            border-radius: 22px;
+            background-color: #fcfcfc;
+            font-size: 13px;
+            color: #a7a7a7;
+            box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.2);
+            margin-right: 15px;
+        }
+        .info {
+            font-size: 12px;
+            color: #919191;
+        }
     }
-    .more span {
-        display: inline-block;
-        width: 7px;
-        height: 7px;
-        border-bottom: 1px solid #cf1b15;
-        border-right: 1px solid #cf1b15;
-        transform: rotate(-45deg);
+    .pagination {
+        padding-left: 30px;
+        margin: 60px 0 30px 0;
     }
-}
 @import '@/style/media.scss';
 </style>
 
--- src/views/policy/project/project.vue
+++ src/views/policy/project/project.vue
@@ -4,25 +4,35 @@
         <div class="title">
             <p class="g_title_style">新搜索</p>
             <p class="satisfied">如果您对结果不满意，请进行其他搜索</p>
-            <el-input placeholder="请输入内容" v-model="val">
-                <el-button slot="append" @click="search">搜索</el-button>
-            </el-input>
+            <el-form
+                :model="ruleForm"
+                :rules="rules"
+                ref="ruleForm"
+                label-width="65px"
+                class="demo-ruleForm"
+            >
+                <el-form-item label="请输入" prop="name">
+                    <el-input placeholder="请输入" v-model="ruleForm.name">
+                        <el-button slot="append" @click="search('ruleForm')">搜索</el-button>
+                    </el-input>
+                </el-form-item>
+            </el-form>
         </div>
 
         <!-- 间隔线 -->
         <p class="line"></p>
 
         <!-- 搜索结果列表 -->
-        <div class="lists">
+        <div class="lists" v-if="list.length">
             <p class="title_res">{{totalCount}} 搜索结果：</p>
             <template v-if="list.length">
-                <section class="single"  v-for="(item,index) in list" :key="index">
-                        <div class="index">{{index+1}}</div>
-                        <div class="info" @click="goList(item.id)">
-                            <!-- <p class="g_title_style">{{item.custName}}-{{item.registeredCapitalName}}</p> -->
-                            <p class="g_title_style">{{item.custName}}</p>
-                            <p>{{item.createTime}}</p>
-                        </div>
+                <section class="single" v-for="(item,index) in list" :key="index">
+                    <div class="index">{{index+1}}</div>
+                    <div class="info" @click="goList(item.id)">
+                        <!-- <p class="g_title_style">{{item.custName}}-{{item.registeredCapitalName}}</p> -->
+                        <p class="g_title_style">{{item.custName}}</p>
+                        <p>{{item.createTime}}</p>
+                    </div>
                 </section>
             </template>
         </div>
@@ -37,7 +47,6 @@
                 @current-change="handleCurrentChange"
             ></el-pagination>
         </div>
-
     </div>
 </template>
 
@@ -50,43 +59,62 @@ export default {
     mixins: [mixins],
     data() {
         return {
-            totalCount: 1, //总条数
+            ruleForm: {
+                name: '',//输入框值
+            },
+            totalCount: '', //总条数
             list: [], //数据列表
-            val: '', //输入框值
             page: 1, //页数
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: '请输入公司名称',
+                        trigger: 'blur',
+                    },
+                ],
+            },
         }
     },
     computed: {},
     components: {},
     created() {
         if (this.$route.params.data) {
-            this.val = this.$route.params.data.key
+            this.ruleForm.name = this.$route.params.data.key;
+            this.search('ruleForm');
         }
     },
     mounted() {
-        this.search()
+        
     },
     filters: {},
     methods: {
         // 搜索企业
-        search() {
-            let __data = {
-                key: this.val,
-                page: this.page,
-            }
-            searchCompany(__data)
-                .then(res => {
-                    this.totalCount = res.page.totalCount;
-                    if (res.page.list.length) {
-                        this.list = res.page.list
-                    } else {
-                        this.list=[];
-                        this.$message('没有搜索内容')
+        search(formName) {
+            this.$refs[formName].validate(valid => {
+                if (valid) {
+                    let __data = {
+                        key: this.ruleForm.name,
+                        page: this.page,
                     }
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+                    searchCompany(__data)
+                        .then(res => {
+                            this.totalCount = res.page.totalCount
+                            if (res.page.list.length) {
+                                this.list = res.page.list
+                            } else {
+                                this.list = []
+                                this.$message('没有搜索内容')
+                            }
+                        })
+                        .catch(err => {
+                            console.log(err)
+                        })
+                } else {
+                    this.$message('请输入公司名称')
+                    return false
+                }
+            })
         },
         // 去证书列表页
         goList(id) {
@@ -96,70 +124,73 @@ export default {
             })
         },
         // 切换页面处理
-        handleCurrentChange(val) {
-            this.page=val;
-            this.search();
+        handleCurrentChange(value) {
+            this.page = value
+            this.search()
         },
     },
 }
 </script>
 <style lang="scss" scoped>
-    .className {
-        margin: auto;
-    }
-    .title {
-        max-width: 300px;
-        margin: 50px 0 0 0;
-        padding-left: 30px;
-    }
-    .satisfied {
+.el-input {
+    width: 400px;
+}
+.className {
+    margin: auto;
+}
+.title {
+    max-width: 300px;
+    margin: 50px 0 0 0;
+    padding-left: 30px;
+}
+.satisfied {
+    font-size: 13px;
+    color: #e1e1e1;
+}
+
+.line {
+    background: #e1e1e1;
+    width: 100%;
+    height: 1px;
+    margin: 44px 0 40px 0 !important;
+}
+
+.lists {
+    padding-left: 30px;
+    margin: auto;
+}
+.title_res {
+    font-size: 14px;
+    font-weight: bold;
+    color: #919191;
+    margin-bottom: 24px;
+}
+.single {
+    display: flex;
+    align-items: center;
+    margin-bottom: 40px;
+    cursor: pointer;
+    .index {
+        flex-shrink: 0;
+        width: 44px;
+        height: 44px;
+        line-height: 44px;
+        text-align: center;
+        border-radius: 22px;
+        background-color: #fcfcfc;
         font-size: 13px;
-        color: #e1e1e1;
+        color: #a7a7a7;
+        box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.2);
+        margin-right: 15px;
     }
-
-    .line {
-        background: #e1e1e1;
-        width: 100%;
-        height: 1px;
-        margin: 44px 0 40px 0 !important;
-    }
-
-    .lists {
-        padding-left: 30px;
-        margin: auto;
-    }
-    .title_res {
-        font-size: 14px;
-        font-weight: bold;
+    .info {
+        font-size: 12px;
         color: #919191;
-        margin-bottom: 24px;
     }
-    .single {
-        display: flex;
-        align-items: center;
-        margin-bottom: 40px;
-        cursor: pointer;
-        .index {
-            flex-shrink: 0;
-            width: 44px;
-            height: 44px;
-            line-height: 44px;
-            text-align: center;
-            border-radius: 22px;
-            background-color: #fcfcfc;
-            font-size: 13px;
-            color: #a7a7a7;
-            box-shadow: 0px 0px 1px 0px rgba(0, 0, 0, 0.2);
-            margin-right: 15px;
-        }
-        .info {
-            font-size: 12px;
-            color: #919191;
-        }
-    }
-    .pagination {
-        padding-left: 30px;
-        margin: 60px 0 30px 0;
-    }
-    @import '@/style/media.scss';
+}
+.pagination {
+    padding-left: 30px;
+    margin: 60px 0 30px 0;
+}
+@import '@/style/media.scss';
 </style>
